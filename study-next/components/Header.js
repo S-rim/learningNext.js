9fb5d88e281b0815ce8a80cdd0c3931536e000a8
Srim <deamoon0801@naver.com>
2021-03-10 17:10:46 +0900

    server or client

--- study-next/components/Header.js
+++ study-next/components/Header.js
@@ -13,6 +13,9 @@ const Header = () => {
       <Link href="/about">
         <a style={linkStyle}>about</a>
       </Link>
+      <Link href="/ssr-test">
+        <a style={linkStyle}>SSR 테스트</a>
+      </Link>
     </div>
   );
 };
