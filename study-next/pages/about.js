2b1e270633cc64f21ea013616af924139aa84523
Srim <deamoon0801@naver.com>
2021-03-10 17:42:43 +0900

    next.js styling

--- study-next/pages/about.js
+++ study-next/pages/about.js
@@ -2,6 +2,13 @@ import Layout from "../components/Layout";
 
 const About = () => (
   <Layout>
+    <style jsx>
+      {`
+        h2 {
+          color: purple;
+        }
+      `}
+    </style>
     <h2>About Page</h2>
   </Layout>
 );
